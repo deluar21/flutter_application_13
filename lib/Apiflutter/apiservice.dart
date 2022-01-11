50637a903affa9d5b0182d94404ae24335e4008b
Md Deluar Hossen <92197687+deluar21@users.noreply.github.com>
2022-01-11 22:28:59 +0600

    Delete apiservice.dart

--- lib/Apiflutter/apiservice.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert';
-
-mixin ApiService {
-  Future getAllproduct() async {
-    final allproductUrl = Uri.file("https://fakestoreapi.com/products");
-    final response = await http.get(allproductUrl);
-    return jsonDecode(response.body);
-  }
-}
